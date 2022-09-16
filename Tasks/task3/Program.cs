ba3406d950f2270995437e71cac4cded7c570b40
Kristina <kristisha_m@mail.ru>
2022-09-16 19:18:22 +0300

    add task3 in new console project

---
+++ Tasks/task3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Пользователь вводит 8 чисел. Поместите их в массив и распечатайте.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] InputArray()
+{
+Console.WriteLine("Введите 8 целых чисел");
+string abcdefgh = Console.ReadLine()!;
+string [] parts = abcdefgh.Split("");
+int[] array = new int [parts.Length];
+
+for (int i=0; i < parts.Length; i++ )
+{
+    array[i] = int.Parse(parts[i]);
+}
+return array;
+}
+
+void PrintArray(int[] array)
+
+{
+    Console.Write("Ваш массив : ");
+    foreach (int a in array)
+    {
+        Console.Write($"[{a}]");
+    }
+}
+
+int[] arrayTest = InputArray();
+PrintArray(arrayTest);
+
